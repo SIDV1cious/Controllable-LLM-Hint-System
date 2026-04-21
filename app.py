a90e3c5ad39764a3d49220748f2d2c9009629fde
SIDV1cious <1926985814@qq.com>
2026-04-21 17:19:02 +0800

    更新app.py的核心代码与公式键盘功能

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def render_mathlive_input(default_value=''):
     </html>
     '''
 
-    components.html(html_code, height=1000, scrolling=True)
+    components.html(html_code, height=500, scrolling=True)
 
 
 def sync_user_data(username: str):
