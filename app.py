fb023f8e62bc50c545608ec00a1ff7935bbd67e3
SIDV1cious <1926985814@qq.com>
2026-05-21 08:19:26 +0800

    Touch Streamlit entrypoint for redeploy

--- app.py
+++ app.py
@@ -2,5 +2,7 @@
 
 from controlled_hint_system_app import run_controlled_hint_system
 
+STREAMLIT_DEPLOY_MARKER = "2026-05-21-c-code-context"
+
 if __name__ == "__main__":
     run_controlled_hint_system()
